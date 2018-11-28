b1eaa70637b4c71b75f61ce7b37307b5b1f4f5d0
Mano Thanabalan <mano.thanabalan@gmail.com>
2018-11-28 16:24:13 +0800

    Update setup_sync.sh

--- app/comsave/setup_sync.sh
+++ app/comsave/setup_sync.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # mc config host add s3 $S3FS_ENDPOINT $S3FS_ACCESSKEY $S3FS_SECRETKEY
-mkdir /root/.mc
-touch /root/.mc/config.json
+/usr/bin/mc config host add myminio http://$S3FS_ENDPOINT $S3FS_ACCESSKEY $S3FS_SECRETKEY
+#mkdir /root/.mc
+#touch /root/.mc/config.json
 
-cat > /root/.mc/config.json <<EOF
-{
-	"version": "9",
-	"hosts": {
-		"s3": {
-			"url": "$S3FS_ENDPOINT",
-			"accessKey": "$S3FS_ACCESSKEY",
-			"secretKey": "$S3FS_SECRETKEY",
-			"api": "S3v4",
-			"lookup": "dns"
-		}
-	}
-}
-EOF
+#cat > /root/.mc/config.json <<EOF
+#{
+#	"version": "9",
+#	"hosts": {
+#		"s3": {
+#			"url": "$S3FS_ENDPOINT",
+#			"accessKey": "$S3FS_ACCESSKEY",
+#			"secretKey": "$S3FS_SECRETKEY",
+#			"api": "S3v4",
+#			"lookup": "dns"
+#		}
+#	}
+#}
+#EOF
